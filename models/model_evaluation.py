f86053b605ed34bfd884bf1d5af86ffaf69b2fc0
Prayag Padwal <65147413+prayagpadwal@users.noreply.github.com>
2024-07-06 18:21:53 -0700

    Update model_evaluation.py

--- models/model_evaluation.py
+++ models/model_evaluation.py
@@ -1,1 +1,11 @@
+import pandas as pd
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+import pickle
+
+# Load test data and model
+df = pd.read_csv('data/dataset.csv')
+model = pickle.load(open('models/recommendation_model.pkl', 'rb'))
+
+# Evaluate model
+# [Include evaluation steps here]
 
